7d446eb043039830abc40bc814261939a5c1f7d9
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2018-10-18 08:37:04 +0200

    Heroku init

---
+++ heroku.py
@@ -0,0 +1,11 @@
+import os
+from bottle import route, static_file, run
+
+@route('/')
+def show_def():
+    return static_file('test.txt', root=('.'))
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080)
